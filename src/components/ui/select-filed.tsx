967e5168497bd67f291445a83e2f8b7c1966bd18
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-28 15:38:59 +0530

    feat: add select field component

---
+++ src/components/ui/select-filed.tsx
@@ -0,0 +1,32 @@
+import { OptionType } from "@/types";
+import { Form, Select } from "antd";
+import { Rule } from "antd/es/form";
+
+interface PropType {
+  options: OptionType[];
+  name: string;
+  fieldRules: Rule[];
+  isLoading: boolean;
+  placeholder: string;
+}
+
+const SelectField = ({
+  options,
+  name,
+  fieldRules,
+  isLoading,
+  placeholder,
+}: PropType) => {
+  return (
+    <Form.Item name={name} rules={fieldRules}>
+      <Select
+        placeholder={placeholder}
+        style={{ width: "100%" }}
+        options={options}
+        loading={isLoading}
+      />
+    </Form.Item>
+  );
+};
+
+export default SelectField;
